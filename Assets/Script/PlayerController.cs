dc87a06bf84f88c9cd5b84b483a5814b246546a1
Milee <120452281+Medve567@users.noreply.github.com>
2024-01-11 10:25:22 +0100

    asd

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float maxSpeed = 7;
+
+    private Animator animator;
+    private Rigidbody2D rigidbody2D;
+
+    private bool isFacingRight;
+
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        isFacingRight = true;
+    }
+
+   
+    private void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        animator.SetFloat("speed", Mathf.Abs(move));
+
+        rigidbody2D.velocity = new(
+            x: move = maxSpeed,
+            y: rigidbody2D.velocity.y);
+        if ((move > 0 && !isFacingRight) || (move < 0 && isFacingRight))Flip();
+    }
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.localScale = new(
+            x: transform.localScale.x * -1,
+            y: transform.localScale.y);
+    }
+}
